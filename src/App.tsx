92b6eaa98d2ae5202c62a7f3e26cc4994ce99212
DESKTOP-NLUVOF2\user <saqlainzafar342176@gmail.com>
2022-12-30 14:42:12 +0500

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import AdvatismentWidget from "./components/Advatisment/AdvatismentWidget";
+import GreenFooter from "./components/Footer/GreenFooter";
 import Local from "./components/LocalPic/Local";
 import ScopeSpeedService from "./components/ScopeSpeed/ScopeSpeedService";
 import ServiceCard from "./components/ServiceCards/ServiceCard";
@@ -15,6 +16,7 @@ function App() {
       <Services1 />
       <Services2 />
       <Local />
+      <GreenFooter/>
     </div>
   );
 }
